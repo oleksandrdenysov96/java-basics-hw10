b949e3805c99a41bc15a5ede208e14a7a9f841c0
Oleksandr Denysov <denisovalexsd@gmail.com>
2020-04-27 01:22:18 +0300

    Done HW10

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,8 +1,8 @@
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
-
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 public class ExampleExceptionTest {
 
@@ -14,9 +14,10 @@ public class ExampleExceptionTest {
   @DataProvider(name = "data")
   public static Object[][] data() {
     return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
+            {2, 2, 4},
+            {2, 3, 6},
+            {3, 3, 9},
+            {8, 8, 64}
     };
   }
 
@@ -28,20 +29,27 @@ public class ExampleExceptionTest {
   @DataProvider(name = "negativeData")
   public static Object[][] negativeData() {
     return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
+            {-2, 2},
+            {2, -2},
+            {-5, -5},
+            {0, 10}
     };
   }
 
   @Test(dataProvider = "data")
-  public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
+  public void testRectangleArea(int a, int b, int expectedResult) {
+    int actualResult = ExampleException.rectangleArea(a, b);
+    Assert.assertEquals(actualResult, expectedResult);
   }
 
 
   @Test(dataProvider = "negativeData")
   public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
+    String expectedResult = "input value is below zero!";
+    try {
+      int actualResult = ExampleException.rectangleArea(a, b);
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(e.getMessage(), expectedResult);
+    }
   }
 }
